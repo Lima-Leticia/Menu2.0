a53e39663acb0ad38f5cf348cdba98c76190cce0
Letícia Lima <71621821+Lima-Leticia@users.noreply.github.com>
2023-07-06 15:43:59 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,47 @@
-"use client";
-import React, { useState, useEffect } from "react";
+'use client'
+import { useState, useEffect } from "react";
 import Menu from "../app/Menu";
 import Categories from "../app/Categories";
 import FoodModalForm from "../components/FoodModalForm";
-import data from "../json/data"
-import logo from '../logo.jpg';
+import data from "../json/data.json";
+import logo from './logo.jpg';
 import './index.css';
+
 const Home = () => {
-  
   const [menuItems, setMenuItems] = useState(data.menu);
   const [activeCategory, setActiveCategory] = useState("");
   const [categories, setCategories] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
       const response = await fetch('http://localhost:3001/menu');
       const data = await response.json();
-  
       setCategories(data);
     };
-  
+
     loadData();
   }, []);
-  
 
   const filterItems = (category) => {
     setActiveCategory(category);
-    if (category === "tudo") {
-      setMenuItems(items);
+    if (category === "Tudo") {
+      setMenuItems(data.menu);
       return;
     }
-    const newItems = items.filter((item) => item.category === category);
+    const newItems = data.menu.filter(
+      (item) => item.category.toLowerCase() === category.toLowerCase()
+    );
     setMenuItems(newItems);
   };
+  
+  
+
+  const addFood = (food) => {
+    setMenuItems((prevMenuItems) => [...prevMenuItems, food]);
+    setModalOpen(false);
+  };
+  
   return (
     <main>
       <section className="menu section">
@@ -46,24 +55,19 @@ const Home = () => {
           activeCategory={activeCategory}
           filterItems={filterItems}
         />
-         <Menu items={menuItems} />
+        <Menu items={menuItems} />
       </section>
-     
+
       <div>
-        {
-          categories.map( (category, index) => {
-            
-            return(
-              <div key={index}>{category.title}</div>
-            );
-          })
-        }
+        {categories.map((category, index) => {
+          return <div key={index}>{category.title}</div>;
+        })}
       </div>
-      <div className='container mx-auto lg:max-w-screen-lg'>
-      <FoodModalForm />
+      <div className="container mx-auto lg:max-w-screen-lg">
+      <FoodModalForm show={modalOpen} onAddFood={addFood} /> 
+      <button onClick={() => setModalOpen(true)}>Adicionar Comida</button>
       </div>
     </main>
-    
   );
 };
 
